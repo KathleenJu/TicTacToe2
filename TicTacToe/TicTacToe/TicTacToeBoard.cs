786581105030417688dc692296647a19fd386691
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-07-17 09:41:17 +1200

    restructured folders, added more tests and functions to the board class

--- TicTacToe/TicTacToe/TicTacToeBoard.cs
+++ TicTacToe/TicTacToe/TicTacToeBoard.cs
@@ -10,32 +10,45 @@ namespace TicTacToe
         private List<Coordinates> PlayedCoordinates; 
         private int BoardHeight;
         private int BoardWidth;
+        private GameStatus GameStatus;
 
         public TicTacToeBoard(int boardHeight, int boardWidth)
         {
             BoardHeight = boardHeight;
             BoardWidth = boardWidth;
             PlayedCoordinates = new List<Coordinates>();
+            GameStatus = GameStatus.PLAYING;
         }
 
         public bool PlayPiece(Coordinates coordinates)
         {
-            throw new System.NotImplementedException();
+            if (IsEmptyPosition(coordinates))
+            {
+                PlayedCoordinates.Add(coordinates);
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsEmptyPosition(Coordinates coordinates)
+        {
+            return !PlayedCoordinates.Contains(coordinates);
         }
 
         public GameStatus GetGameStatus()
         {
-            throw new System.NotImplementedException();
+            return GameStatus;
         }
 
         public void StartGame()
         {
-            throw new System.NotImplementedException();
+            ResetBoard();
         }
 
         public bool ResetBoard()
         {
             PlayedCoordinates.Clear();
+            GameStatus = GameStatus.OVER;
             return PlayedCoordinates.Count == 0;
         }
 
--- TicTacToe/TicTacToeTests/TicTacToeBoardShould.cs
+++ TicTacToe/TicTacToeTests/TicTacToeBoardShould.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using TicTacToe;
 using Moq;
+using TicTacToe.Enum;
 using Xunit;
 
 namespace TicTacToeTests
@@ -9,7 +10,7 @@ namespace TicTacToeTests
     public class TicTacToeBoardShould
     {
         [Fact]
-        public void PutCoordinatesInTheCorrectPositionOnTheBoard()
+        public void ResetTheBoardAndClearThePlayedCordinates()
         {
             var game = new TicTacToeBoard(3, 3);
             var actualOutput = game.ResetBoard();
@@ -17,6 +18,40 @@ namespace TicTacToeTests
 
             Assert.Equal(expectedOutput, actualOutput);
             Assert.Empty(game.GetPlayedCoordinates());
+        }
+        
+        [Fact]
+        public void PlayThePlayersTurnIfPositionIsNotTaken()
+        {
+            var game = new TicTacToeBoard(3, 3);
+            var coordinates = new Coordinates(1,1);
+            var actualOutput = game.PlayPiece(coordinates);
+            var expectedOutput = true;
+
+            Assert.Equal(expectedOutput, actualOutput);
+            Assert.True(game.GetPlayedCoordinates().Contains(coordinates));
+        }
+        
+        [Fact]
+        public void NotPlayThePlayersTurnIfPositionIsTaken() //not sure how to test this
+        {
+            var game = new TicTacToeBoard(3, 3);
+            var coordinates = new Coordinates(1,1);
+            var firstPlayPiece = game.PlayPiece(coordinates);
+            var actualOutput = game.PlayPiece(coordinates);
+            var expectedOutput = false;
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+        
+        [Fact]
+        public void ReturnGameStatusPlaying() //not sure how to test this
+        {
+            var game = new TicTacToeBoard(3, 3);
+            var actualOutput = game.GetGameStatus();
+            var expectedOutput = GameStatus.PLAYING;
+
+            Assert.Equal(expectedOutput, actualOutput);
         }
     }
 }
