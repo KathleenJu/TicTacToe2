8d12f7114d324d09d7c238f823d7971601ec6d3f
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-07-16 14:39:20 +1200

    set up board class

---
+++ TicTacToe/TicTacToe/TicTacToeBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    public class TicTacToeBoard
+    {
+        private List<Coordinates> GameBoard;
+        private int BoardHeight;
+        private int BoardWidth;
+
+        public TicTacToeBoard(int boardHeight, int boardWidth)
+        {
+            BoardHeight = boardHeight;
+            BoardWidth = boardWidth;
+            SetUpGameBoard();
+        }
+
+        public void SetUpGameBoard()
+        {
+            
+        }
+    }
+}
